479979791de203a20dd30a639c0bfa51e0f6b85c
Raina Ross <rross2601@gmail.com>
2025-10-07 16:52:17 -0600

    class work

---
+++ EclipseWorkspace/CS2050_ClassWork/M15_Class.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+/**
+ * 
+ */
+public class M15_Class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the year: ");
+		// read integer from keyboard for year
+		int year = input.nextInt();
+	}
+
+}
